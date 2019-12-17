3a5937c2f7b14525fda96e8a61f354e9c6f6c225
Mauricio Neto <maumneto@gmail.com>
2019-12-17 14:51:59 -0300

    questao a mais

--- lista-exercicio-1/questao-18.c
+++ lista-exercicio-1/questao-18.c
@@ -3,3 +3,13 @@
  * iniciando em 10 e terminando em 0. Mostrar uma mensagem “FIM” após a contagem.
 */
 #include <stdio.h>
+
+int main ()
+{
+    int valor = 10;
+    while (valor >= 0){
+        printf("Valor %d", valor);
+        valor--;
+    }
+    return 0;
+}
