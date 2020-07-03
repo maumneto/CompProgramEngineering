50e81ca53e26c93aec2270a187deceba079a2d03
Mauricio Neto <maumneto@gmail.com>
2020-07-03 07:13:11 -0300

    codigo da aula

---
+++ codigos-aula/aula1.c
@@ -0,0 +1,15 @@
+// comentario de linha
+/**
+ * comentarios
+ * de
+ * multiplas
+ * linhas
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Olá Mundo Cruel");
+    return 0;
+}
