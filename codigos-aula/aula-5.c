06b3a892bfcf6c724953a9e1438fad35d1354a8d
Mauricio Neto <maumneto@gmail.com>
2020-07-06 13:31:56 -0300

    add codigo

--- codigos-aula/aula-5.c
+++ codigos-aula/aula-5.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-// Casting (Conversão) de valores
+// Cast (Conversão) de valores
 int main(){
-    int x = 65, y = 2, z;
+    int x = 65, y = 2, z, k;
     float f, f1;
-    char c;
+    char c, l = 'a';
 
     f = (float) x;
     c = (char) x;
     f1 = (float) x/y;
     z = (int) f1;
+    k = (int) l;
+
     printf("O valor da variável f é: %f\n", f);
     printf("O valor da variável c é: %c\n", c);
     printf("O valor da variável f1 é: %f\n", f1);
     printf("O valor da variável z é: %d\n", z);
+    printf("O valor da variável k é: %d\n", k);
 
     return 0;
 }
