cf13eaba4e144c52ab22e4a35556455c95d10041
maumneto <maumneto@gmail.com>
2020-09-28 20:08:21 -0300

    project jokenpo

---
+++ JokenpoSimples/main.c
@@ -0,0 +1,100 @@
+jokenpio/**
+ * @name Jokenpo Simples!
+ * @author Maurício Moreira Neto
+ * Este projeto simula o jogo pedra-papel-tesoura
+ * Desenvolvido orgulhosamente C.
+ * */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+
+int main() 
+{
+    setlocale(LC_ALL, "portuguese");
+    int user, computer;
+
+    // tela de inicial do jogo!
+    system("clear");
+    printf("-------------------------------------------- \n");
+    printf("BEM VINDO AO JOGO PEDRA-PAPEL-TESOURA - PCE\n");
+    printf("--------------------------------------------\n");
+    printf("\n");
+
+    // entrada da opção do usuário!
+    do{
+        printf("Escolha a Opção:\n[0]-Pedra\n[1]-Papel\n[2]-Tesoura\nJogador: ");
+        scanf("%d", &user);
+        if (user < 0 || user >2) 
+        {
+            printf("Escolha Errada!\n");
+            printf("\n");
+        }
+    }while(user < 0 || user > 2);
+
+    // gerar a opção do computador!
+    srand(time(NULL));
+    computer = rand() % 3;
+    
+    printf("\n");
+    printf("Jogador --> %d\n", user);
+    printf("Computador --> %d\n", computer);
+    printf("\n");
+
+    // Implementando a lógica do jogo!
+    if (user == 0)
+    {
+        if (computer == 0) 
+        {
+            printf("Empatou!\n");
+        } 
+        else if (computer == 1)
+        {
+            printf("Você Perdeu!\n");
+        } 
+        else
+        {
+            printf("Você Ganhou!\n");
+        }
+    }
+    else if (user == 1)
+    {
+        if (computer == 0)
+        {
+            printf("Você Ganhou!\n");
+        } 
+        else if (computer == 1)
+        {
+            printf("Empatou!\n");
+        } 
+        else 
+        {
+            printf("Você Perdeu!\n");  
+        }
+    }
+    else 
+    {
+        if (computer == 0)
+        {
+            printf("Você Perdeu!\n");
+        } 
+        else if (computer == 1)
+        {
+            printf("Você Ganhou!\n");
+        }
+        else
+        {
+            printf("Empatou!\n");
+        }
+    }
+
+    // tela de saída do jogo!
+    printf("\n");
+    printf("-------------------------------------------- \n");
+    printf(" OBRIGADO POR JOGAR! VOLTE SEMPRE! \n");
+    printf("--------------------------------------------\n");
+    printf("\n");
+
+    return EXIT_SUCCESS;
+}
